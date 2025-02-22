e76a2379604a7b869e87fec84a09c84e20cc1936
sartaj <hajamsaj@gmail.com>
2025-02-22 21:36:57 +0530

    updated components and basic strcuture

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'secondary': {
+          900: '#2A2A2A'
+        }
+      }
+    }
+  },
+  plugins: []
+} 
